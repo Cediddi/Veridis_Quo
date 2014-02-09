92abfdd36ecaf6cbae6266d87174102a00ea67a3
Umut Karci <umutkarci@std.sehir.edu.tr>
2014-02-09 12:21:26 +0200

    support __iter__

--- VeridisQuo/Veridis.py
+++ VeridisQuo/Veridis.py
@@ -27,6 +27,15 @@ class Veridis:
         cls.load(str(item)+".vd", delete=True)
 
     @classmethod
+    def __iter__(cls):
+        data_map = cls._load_map(cls.map_file)
+        output = []
+        for data_file, data_type in data_map:
+            data = decompress(cls._load_data(data_file)).decode("UTF-8")
+            output.append(cls.primitive_types[data_type](data))
+        return iter(output)
+
+    @classmethod
     def append(cls, value):
         return cls.dump(value)
 
